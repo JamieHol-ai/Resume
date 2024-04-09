7a9a7d239db43a372b16df7527274757d5d436bb
Jamie Holroyde <jamie_holroyde@icloud.com>
2024-04-09 01:49:31 +0100

    Update PDF file path and download link in resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -5,13 +5,14 @@ export default function Resume() {
   return (
     <Layout>
       <iframe 
-        src="../../public/resume.pdf" 
+        src="./resume.pdf" 
         style={{width:'100%', height:'800px'}} 
         frameborder="0" 
         allowFullScreen/>
       <p>
         If you cannot view the file, please download it: 
         <a href="../../resume.pdf">Download PDF</a>
+        <a href="JamieHol-ai.github.io/public/resume.pdf" target="_blank">PDF.</a>
       </p>
     </Layout>
   );
