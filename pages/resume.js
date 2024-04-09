5e8a70ed3050724c859c74c6e30d6c484ed04cfc
Jamie Holroyde <jamie_holroyde@icloud.com>
2024-04-09 01:05:50 +0100

    Refactor resume page layout and add PDF download link

--- pages/resume.js
+++ pages/resume.js
@@ -1,21 +1,19 @@
-import Head from 'next/head';
-import Link from 'next/link';
+// Resume.js
 import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import Navbar from '../components/navbar';
-
 
 export default function Resume() {
-    return (
-        <Layout>
-            <iframe 
+  return (
+    <Layout>
+      <iframe 
         src="/resume.pdf" 
         style={{width:'100%', height:'800px'}} 
         frameborder="0" 
-        allowFullScreen>
-        This browser does not support PDFs. Please download the PDF to view it: 
+        allowFullScreen
+      />
+      <p>
+        If you cannot view the file, please download it: 
         <a href="/resume.pdf">Download PDF</a>
-      </iframe>
-        </Layout>
-    );
+      </p>
+    </Layout>
+  );
 }
