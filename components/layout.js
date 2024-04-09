3e49702837539f15eee2f123cd3fa6b90b7843ee
Jamie Holroyde <jamie_holroyde@icloud.com>
2024-04-09 02:02:08 +0100

    Update favicon and image paths in layout.js

--- components/layout.js
+++ components/layout.js
@@ -12,7 +12,7 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/resume/favicon.ico" />
         <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
@@ -32,7 +32,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="./images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -45,7 +45,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="/images/profile.jpg"
+                src="../images/profile.jpg"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
